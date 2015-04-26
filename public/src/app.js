0b88e421d27630ce759c28075c52a75053b48913
KyleAsaff <kyleasaff@gmail.com>
2015-04-26 16:15:25 -0300

    Form Field Directive Created

--- public/src/app.js
+++ public/src/app.js
@@ -1,4 +1,4 @@
-angular.module('ContactsApp', ['ngRoute', 'ngResource'])
+angular.module('ContactsApp', ['ngRoute', 'ngResource', 'ngMessages'])
 	.config(function($routeProvider, $locationProvider) {
 		$routeProvider
 			.when('/contacts', {
