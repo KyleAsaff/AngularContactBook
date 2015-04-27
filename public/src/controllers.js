047fd9659d61fbc6d41d631f670d5900ac7fb7e1
KyleAsaff <kyleasaff@gmail.com>
2015-04-27 14:39:23 -0300

    Created Nav Bar

--- public/src/controllers.js
+++ public/src/controllers.js
@@ -1,5 +1,6 @@
 angular.module('ContactsApp')
-	.controller('ListController', function ($scope, Contact, $location) {
+	.controller('ListController', function ($scope, $rootScope, Contact, $location) {
+		$rootScope.PAGE = "all";
 		$scope.contacts = Contact.query();
 		$scope.fields = ['firstName', 'lastName'];
 
@@ -15,7 +16,8 @@ angular.module('ContactsApp')
 			$location.url('/contact/'+id);
 		};
 	})
-	.controller('NewController', function ($scope, Contact, $location) {
+	.controller('NewController', function ($scope, $rootScope, Contact, $location) {
+		$rootScope.PAGE = "new";
 		$scope.contact = new Contact({
 			firstName:  ['', 'text'],
 			lastName: ['', 'text'],
@@ -36,7 +38,8 @@ angular.module('ContactsApp')
 			}
 		};
 	})
-	.controller('SingleController', function ($scope, $location, Contact, $routeParams) {
+	.controller('SingleController', function ($scope, $rootScope, $location, Contact, $routeParams) {
+		$rootScope.PAGE = "single";
 		$scope.contact = Contact.get({ id: parseInt($routeParams.id, 10) });
 		$scope.delete = function () {
 			$scope.contact.$delete();
