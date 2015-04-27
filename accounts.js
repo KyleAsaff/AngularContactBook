0dc7673b8dc07ad970802d75669c7f8f146872cb
KyleAsaff <kyleasaff@gmail.com>
2015-04-27 17:58:33 -0300

    Implemented Settings Page

--- accounts.js
+++ accounts.js
@@ -60,6 +60,19 @@ router
 			});
 		}
 		next();
+	})
+	.get('/options/displayed_fields', function (req, res) {
+		if(!req.user) {
+			res.json([]);
+		} else {
+			res.json(req.user.options.displayed_fields || []);
+		}
+	})
+	.post('/options/displayed_fields', function (req, res) {
+		req.user.options.displayed_fields = req.body.fields;
+		db.update({ id: req.user.id }, req.user, function (err, data) {
+			res.json(data[0].options.displayed_fields);
+		});
 	});
 
 module.exports = router;
--- public/src/controllers.js
+++ public/src/controllers.js
@@ -1,8 +1,8 @@
 angular.module('ContactsApp')
-	.controller('ListController', function ($scope, $rootScope, Contact, $location) {
+	.controller('ListController', function ($scope, $rootScope, Contact, $location, options) {
 		$rootScope.PAGE = "all";
 		$scope.contacts = Contact.query();
-		$scope.fields = ['firstName', 'lastName'];
+		$scope.fields = ['firstName', 'lastName'].concat(options.displayed_fields);
 
 		$scope.sort = function (field) {
 			$scope.sort.field = field;
@@ -44,5 +44,26 @@ angular.module('ContactsApp')
 		$scope.delete = function () {
 			$scope.contact.$delete();
 			$location.url('/contacts');
+		};
+	})
+	.controller('SettingsController', function ($scope, $rootScope, options, Fields) {
+		$rootScope.PAGE = 'settings';
+
+		$scope.allFields = [];
+		$scope.fields = options.displayed_fields;
+
+		Fields.headers().then(function (data) {
+			$scope.allFields = data;
+		});
+
+		$scope.toggle = function (field) {
+			var i = options.displayed_fields.indexOf(field);
+			if(i > -1) {
+				options.displayed_fields.splice(i, 1);
+			} else {
+				options.displayed_fields.push(field);
+			}
+
+			Fields.set(options.displayed_fields);
 		};
 	});
